de49ef3c76227efb52f236c0a507ab52d5a636a7
SoftwareAdelphi <44646026+SoftwareAdelphi@users.noreply.github.com>
2018-11-09 11:25:33 -0500

    Create SysAdminQueue.php

---
+++ SysAdminQueue.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>System Administrator Queue</title>
+</head>
+
+
+<body>
+<h1>System Administrator Tickets:</h1>
+<form action="SysAdminQueue.php" method="post">
+<table>
+        <tr>
+                <td>Enter an order number:</td>
+                <td><input type="text" size="10" name="number"/></td>
+        </tr>
+</table>
+<p><input type="submit"></p>
+</form>
+
+<?php
+
+//open connection
+try {
+
+        $dbh = new PDO('pgsql:dbname= kaylapollock');
+
+} catch (PDOException $e) {
+        print "Error: ".$e->getMessage()."<br/>";
+        die();
+}
+
+//store value
+$i=1;
+echo "<PRE>";
+if (array_key_exists("number", $_REQUEST)) {
+	//begin parameterized query ? is placeholder
+	$st = $dbh->prepare("SELECT * FROM ___ WHERE asignee = sysadmin");
+
+	//map each ? to a variable
+	$st->bindParam(1, $_REQUEST['number']);
+
+//        $number = $_REQUEST['number'];
+} else {
+	$st = $dbh->prepare("SELECT * FROM ___");
+//        $number = '';
+}
+
+//execute query
+$res = $st->execute();
+print "<pre>
+";
+?>
+
+Ticket ID	Issue Title	Description	Contact First Name	Last Name	Telephone	Email	Status Asignees
+
+<?php
+#$res = $dbh->query($q, PDO::FETCH_ASSOC);
+while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+	foreach ($row as $key=>$value) {
+		print "$value\t";
+	}
+	print "\n";
+}
+$dbh = null;
+?>
+
+</body>
+</html>
