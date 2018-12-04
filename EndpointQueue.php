713467892a0c9666da7af7280ff5b9bb7046d351
SoftwareAdelphi <44646026+SoftwareAdelphi@users.noreply.github.com>
2018-12-03 20:23:45 -0500

    Update EndpointQueue.php

--- EndpointQueue.php
+++ EndpointQueue.php
@@ -16,36 +16,29 @@
 </form>
 
 <?php
-
 //open connection
         $host = "localhost";
         $user = "proggadeb";
         $pw = "";
         $database = "proggadeb";
-
         $dbh = new mysqli($host, $user, $pw, $database);
         if ($dbh->connect_errno) {
                 echo "Connect failed: ". $dbh->connect_error;
                 exit();
 	}
-
-
 //store value
 $i=1;
 echo "<PRE>";
 if (array_key_exists("number", $_REQUEST)) {
 	//begin parameterized query ? is placeholder
-	$st = $dbh->prepare("SELECT * FROM Tckets WHERE number =?");
-
+	$st = $dbh->prepare("SELECT * FROM Tickets WHERE number =?");
 	//map each ? to a variable
 //	$st->bindParam(1, $_REQUEST['number']);
-
 //        $number = $_REQUEST['number'];
 } else {
 	$st = $dbh->prepare("SELECT * FROM Tickets");
 //        $number = '';
 }
-
 //execute query
 //$res = $st->execute();
 print "<pre>
@@ -54,19 +47,18 @@ print "<pre>
 
 
 <?php
-
-$sql = "SELECT * FROM `Tickets`;";
+$sql = "SELECT * FROM `Tickets` WHERE assignee = 'Endpoint';";
         $result = $dbh->query($sql);
         if(!$result){
 //                echo "Bummer! " . $dbh->error;
         }
         else{
 //                echo "<br>The result has " . $result->num_rows. " rows.";
-
         //two methods
         $table = $result->fetch_all();
         //var_dump($table);
 echo "<table border = '1'>";
+echo "<tr><th>Issue</th><th>Status</th><th>Ticket #</th><th>Date Created</th><th>Description</th><th>Last Name</th><th>First Name</th><th>Phone</th><th>Email</th><th>Username</th><th>Assignee</th></tr>";
         foreach($table as $row){
                 echo "<tr>";
                 foreach($row as $value){
