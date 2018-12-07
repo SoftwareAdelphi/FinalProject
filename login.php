f0792b01b4a6b42c4a04f52a8ba20cc6a78aaee6
p-deb <proggadeb@mail.adelphi.edu>
2018-12-07 11:25:44 -0500

    update assignee conditions

--- login.php
+++ login.php
@@ -8,13 +8,30 @@
         if (isset($_REQUEST['pass']))
         {       $pass = $_REQUEST['pass'];      }
 
-        $sqlLogin = mysqli_query($db,"SELECT * FROM incidentResponders WHERE username = '$user' AND password = '$pass'");
+        $sqlLogin = mysqli_query($db,"SELECT * FROM Login WHERE username = '$user' AND password = '$pass'");
 
         if (mysqli_num_rows($sqlLogin) == 1)
         {
                 $_SESSION['login_user'] = $user;
-                echo "<script> location.href='Homepage.php'; </script>";
-                exit;
+                //echo "<script> location.href='Homepage.php'; </script>";
+                $role = mysqli_query($db, "SELECT assignee FROM Login WHERE username = '$user' AND password = '$pass';");
+
+		$row = mysqli_fetch_row($role);
+		if ($row[0] == 'Helpdesk') 
+		{
+			header("location:HelpdeskQueue.php");
+		}
+		else if ($row[0] == 'Sysadmin')
+		{
+			header("location:SysAdminQueue.php");
+		}
+		else if ($row[0] == 'Endpoint');
+		{
+			header("location:EndpointQueue.php");
+		}
+//		else
+//		{ echo "This should never display!"; }
+		exit;
         }
         else
         {
