2bbb34e2f9d24335098b320ba6415e7b1577e79d
p-deb <proggadeb@mail.adelphi.edu>
2018-12-07 11:05:00 -0500

    login from DBMS

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+        include("connection.php");      // connects to database
+        session_start();
+
+        if (isset($_REQUEST['user']))
+        {       $user = $_REQUEST['user'];      }
+        if (isset($_REQUEST['pass']))
+        {       $pass = $_REQUEST['pass'];      }
+
+        $sqlLogin = mysqli_query($db,"SELECT * FROM incidentResponders WHERE username = '$user' AND password = '$pass'");
+
+        if (mysqli_num_rows($sqlLogin) == 1)
+        {
+                $_SESSION['login_user'] = $user;
+                echo "<script> location.href='Homepage.php'; </script>";
+                exit;
+        }
+        else
+        {
+                echo "Error: " . $sqlLogin . "<br>" . $db->error;
+        }
+?>
