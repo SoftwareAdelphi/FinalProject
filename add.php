f88c9ef45825ac1ed96b7b7cc3ac9c03eefe01b4
p-deb <45210221+p-deb@users.noreply.github.com>
2018-12-10 15:23:15 -0500

    Update add.php

--- add.php
+++ add.php
@@ -1,58 +1,46 @@
-<html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>Add Issue</title>
-
-    <meta name="author" content="GB">
-
-
-    <script src="javascript.js"></script>
-    <script language="javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.0.1.min.js"></script>
-</head>
+<html lang = "en">
 <body>
-
+<div class="A option">
+<h2>Please fill out every field</h2>
+  <form name="add" action="" method="POST">
+  First Name:    <input type="text" name="fnameadd" /><br>
+  Last Name:     <input type="text" name="lnameadd" /><br>
+  Issue Title:    <input type="text" name="issue_Title" /><br>
+  Description:  <input type="text" name="description" /><br>
+  Telephone Number:          <input type="text" name="telephone" /><br>
+  Email:        <input type="text" name="countyadd" /><br>
+  <!-- Helpdesk is auto assigned. Date/status is auto generated. Issue number is also auto generated-->
+  <input type="submit">
+  </form>
+</div>
+</body>
+</html>
 <?php
-//open connection
-        $host = "localhost";
-        $user = "proggadeb";
-        $pw = "";
-        $database = "proggadeb";
-        $dbh = new mysqli($host, $user, $pw, $database);
-        if ($dbh->connect_errno) {
-                echo "Connect failed: ". $dbh->connect_error;
-                exit();
-	}
+//double checks to see if everything is filled in
+//query from 
+if( (!isset($_POST['fnameadd']) || trim($_POST['fnameadd']) == "") || 
+(!isset($_POST['lnameadd']) || trim($_POST['lnameadd']) == '') || 
+(!isset($_POST['issue_Title']) || trim($_POST['issue_Title']) == '') ||
+(!isset($_POST['telephone']) || trim($_POST['telephone']) == '') ||
+(!isset($_POST['description']) || trim($_POST['description']) == '')
+{
+   echo "You did not fill out the required fields.";
 
+} else {
+	$time = int time ( void ) //grabs timestamp
+	$sql = "INSERT INTO //table (firstname, lastname, issue, description, time, status, assignee ) //all the table column names has to be in order of columns and such should be correct
+VALUES ('$_POST[fnameadd]', '$_POST[lnameadd]','$_POST[issue_Title]', '$_POST[description]', '$time', 'open', 'Helpdesk')"; //doubleCheck this
+}
 
-		//double checks to see if everything is filled in
-	//query from 
-		if( (!isset($_POST['fnameadd']) || trim($_POST['fnameadd']) == "") || 
-			(!isset($_POST['lnameadd']) || trim($_POST['lnameadd']) == '') || 
-			(!isset($_POST['issue_Title']) || trim($_POST['issue_Title']) == '') ||
-			(!isset($_POST['telephone']) || trim($_POST['telephone']) == '') ||
-			(!isset($_POST['description']) || trim($_POST['description']) == ''){
-
-		   echo "You did not fill out the required fields.";
-		
-		} else {
-			$time = int time ( void ) //grabs timestamp
-			$sql = "INSERT INTO //table (firstname, lastname, issue, description, time, status, assignee ) //all the table column names has to be in order of columns and such should be correct
-			VALUES ('$_POST[fnameadd]', '$_POST[lnameadd]','$_POST[issue_Title]', '$_POST[description]', '$time', 'open', 'Helpdesk')"; //doubleCheck this
-		}
-		
-	//Sees if this works out correctly and actually inserted	
+//Sees if this works out correctly and actually inserted
 if ($dbh->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $dbh->error;
 }
-
 $dbh->close();
-
 <br>
-	<input type="button" onclick="location.href='TicketSystemForms.html';" value="Go Back to Menu" />
-
+<input type="button" onclick="location.href='TicketSystemForms.html';" value="Go Back to Menu" />
 </body>
-
 </html>
 ?>
