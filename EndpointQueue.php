374e6dbb957c9be723bf0b3506745a619c28dd66
k-pollock <44646026+k-pollock@users.noreply.github.com>
2018-12-13 18:53:10 -0500

    Update EndpointQueue.php

--- EndpointQueue.php
+++ EndpointQueue.php
@@ -1,74 +1,36 @@
 <html>
 <head>
 <title>Endpoint Queue</title>
-</head>
-
-<body>
-<h1>Endpoint Tickets:</h1>
-<form action="EndpointQueue.php" method="post">
-<table>
-        <tr>
-                <td>Enter a ticket number:</td>
-                <td><input type="text" size="10" name="number"/></td>
-        </tr>
-</table>
-<p><input type="submit"></p>
-</form>
+<center><h1>ENDPOINT QUEUE</h1><br>
 
 <?php
 //open connection
-        $host = "localhost";
-        $user = "proggadeb";
-        $pw = "";
-        $database = "proggadeb";
-        $dbh = new mysqli($host, $user, $pw, $database);
-        if ($dbh->connect_errno) {
-                echo "Connect failed: ". $dbh->connect_error;
-                exit();
-	}
-//store value
-$i=1;
-echo "<PRE>";
-if (array_key_exists("number", $_REQUEST)) {
-	//begin parameterized query ? is placeholder
-	$st = $dbh->prepare("SELECT * FROM Tickets WHERE number =?");
-	//map each ? to a variable
-//	$st->bindParam(1, $_REQUEST['number']);
-//        $number = $_REQUEST['number'];
-} else {
-	$st = $dbh->prepare("SELECT * FROM Tickets");
-//        $number = '';
-}
-//execute query
-//$res = $st->execute();
-print "<pre>
-";
-?>
+include ("connection.php");
 
+	$sql = "SELECT number, title, description, status, assignee, date FROM Tickets WHERE assignee = 'Endpoint' ORDER BY date";
+	$result = mysqli_query($db, $sql);
 
-<?php
-$sql = "SELECT * FROM `Tickets` WHERE assignee = 'Endpoint';";
-        $result = $dbh->query($sql);
-        if(!$result){
-//                echo "Bummer! " . $dbh->error;
-        }
-        else{
-//                echo "<br>The result has " . $result->num_rows. " rows.";
-        //two methods
-        $table = $result->fetch_all();
+	if (!$result){
+        echo "Oops! " . $db->error;
+      }
+      else{
+                $table = $result->fetch_all();
         //var_dump($table);
-echo "<table border = '1'>";
-echo "<tr><th>Issue</th><th>Status</th><th>Ticket #</th><th>Date Created</th><th>Description</th><th>Last Name</th><th>First Name</th><th>Phone</th><th>Email</th><th>Username</th><th>Assignee</th></tr>";
+        echo "<table border = '1'>";
+        echo "<tr><th>Ticket #</th><th>Title</th><th>Description</th><th>Status</th><th>Assignees</th><th>Date Created</th></tr>";
         foreach($table as $row){
                 echo "<tr>";
                 foreach($row as $value){
                         echo "<td>$value</td>";
                 }
                 echo "</tr>";
-		}
+        }
 	}
 ?>
 
-</body>
-</html>
 
+<input type="button" onclick="location.href='home.php';" value="Go Back to Menu" /><br>
+
+</body>
+
+</html>
