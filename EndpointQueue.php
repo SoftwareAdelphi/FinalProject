7ed60e7571647fcc93bd3633ff99665a1c05e466
k-pollock <44646026+k-pollock@users.noreply.github.com>
2018-12-13 22:04:22 -0500

    Commented

--- EndpointQueue.php
+++ EndpointQueue.php
@@ -6,19 +6,23 @@
 <?php
 //open connection
 include ("connection.php");
-
+	//prepare info to display
 	$sql = "SELECT number, title, description, email, status, assignee, date FROM Tickets WHERE assignee = 'Endpoint' ORDER BY date";
 	$result = mysqli_query($db, $sql);
 
+	//nothing prepared --> error
 	if (!$result){
         echo "Oops! " . $db->error;
       }
+	//information to display --> display
       else{
                 $table = $result->fetch_all();
         //var_dump($table);
+	//create table layout for info
         echo "<table border = '1'>";
         echo "<tr><th>Ticket #</th><th>Title</th><th>Description</th><th>Contact</th><th>Status</th><th>Assignees</th><th>Date Created</th></tr>";
-        foreach($table as $row){
+        //iterate through and display info in table
+	foreach($table as $row){
                 echo "<tr>";
                 foreach($row as $value){
                         echo "<td>$value</td>";
@@ -28,8 +32,9 @@ include ("connection.php");
 	}
 ?>
 
-
+<!-- button to log out and return to home -->
 <input type="button" onclick="location.href='home.php';" value="Log Out" />
+<!-- button to log out and return to home -->
 <input type="button" onclick="location.href='update.html';" value="Update Description" /><br>
 
 </body>
